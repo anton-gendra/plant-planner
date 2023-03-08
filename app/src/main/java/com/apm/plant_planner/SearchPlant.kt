33842bd5c5317a95609e52d84ef49b307b443c6f
Julio <julio.ffernandez@udc.es>
2023-03-08 19:55:51 +0100

    Search plant layout

---
+++ app/src/main/java/com/apm/plant_planner/SearchPlant.kt
@@ -0,0 +1,11 @@
+package com.apm.plant_planner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchPlant : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_plant)
+    }
+}
