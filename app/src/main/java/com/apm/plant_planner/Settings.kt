ae234cf4810e1718754e828a5f7f23c161924d8f
anton <gendra.job@gmail.com>
2023-03-10 16:43:00 +0100

    Implemented first button that shows Toast

--- app/src/main/java/com/apm/plant_planner/Settings.kt
+++ app/src/main/java/com/apm/plant_planner/Settings.kt
@@ -2,10 +2,21 @@ package com.apm.plant_planner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.widget.SwitchCompat
 
 class Settings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val switchDarkMode: SwitchCompat = findViewById(R.id.theme_switch)
+        switchDarkMode.setOnClickListener {
+            if (switchDarkMode.isChecked) {
+                Toast.makeText(this, "Dark mode activated", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Dark mode activated", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
