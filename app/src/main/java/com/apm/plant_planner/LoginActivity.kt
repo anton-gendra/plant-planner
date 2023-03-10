3bc9ff096b371df533fde3435536bcd72efb1d1d
anton <gendra.job@gmail.com>
2023-03-10 17:11:21 +0100

    Changed mininum sdk version to 19. Changed launching activity to login. Implemented examples of intent and toast

--- app/src/main/java/com/apm/plant_planner/LoginActivity.kt
+++ app/src/main/java/com/apm/plant_planner/LoginActivity.kt
@@ -1,11 +1,18 @@
 package com.apm.plant_planner
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val registerBtn: Button = findViewById(R.id.register_btn)
+        registerBtn.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
