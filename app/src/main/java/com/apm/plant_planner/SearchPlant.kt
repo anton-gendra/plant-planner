d36296e81b400cf638e0782a00cd00bc702e7f1f
Pablo <pabrod17@gmail.com>
2023-03-23 22:38:48 +0100

    Fix error with SearchPlant view

--- app/src/main/java/com/apm/plant_planner/SearchPlant.kt
+++ app/src/main/java/com/apm/plant_planner/SearchPlant.kt
@@ -5,27 +5,28 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class SearchPlant : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_plant)
-        val plantcardBtn: Button = findViewById(R.id.card_view)
+        val plantcardBtn: ConstraintLayout = findViewById(R.id.card_view)
         plantcardBtn.setOnClickListener {
             Toast.makeText(this, "Select the plant", Toast.LENGTH_LONG).show()
             startActivity(Intent(this, PlantAtributtes::class.java))
         }
-        val plantcard2Btn: Button = findViewById(R.id.card_view2)
+        val plantcard2Btn: ConstraintLayout = findViewById(R.id.card_view2)
         plantcard2Btn.setOnClickListener {
             Toast.makeText(this, "Select the plant", Toast.LENGTH_LONG).show()
             startActivity(Intent(this, PlantAtributtes::class.java))
         }
-        val plantcard3Btn: Button = findViewById(R.id.card_view3)
+        val plantcard3Btn: ConstraintLayout = findViewById(R.id.card_view3)
         plantcard3Btn.setOnClickListener {
             Toast.makeText(this, "Select the plant", Toast.LENGTH_LONG).show()
             startActivity(Intent(this, PlantAtributtes::class.java))
         }
-        val plantcard4Btn: Button = findViewById(R.id.card_view4)
+        val plantcard4Btn: ConstraintLayout = findViewById(R.id.card_view4)
         plantcard4Btn.setOnClickListener {
             Toast.makeText(this, "Select the plant", Toast.LENGTH_LONG).show()
             startActivity(Intent(this, PlantAtributtes::class.java))
