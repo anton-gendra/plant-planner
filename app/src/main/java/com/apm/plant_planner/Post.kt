591ed703941002b14a09b2e46110bd92146a58f7
Pablo <pabrod17@gmail.com>
2023-03-23 19:26:05 +0100

    Add flow post to social windows and new message Post Created

--- app/src/main/java/com/apm/plant_planner/Post.kt
+++ app/src/main/java/com/apm/plant_planner/Post.kt
@@ -1,11 +1,20 @@
 package com.apm.plant_planner
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class Post : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post)
+
+        val registerBtn: Button = findViewById(R.id.button6)
+        registerBtn.setOnClickListener {
+            Toast.makeText(this, "Post created", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, Social::class.java))
+        }
     }
 }
