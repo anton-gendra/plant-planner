9506494a7380af2c7bc5387d6df902edf3a236e6
agr17 <aregueiro1700@gmail.com>
2023-03-23 10:19:00 +0100

    Flujo pantalla Log In y correcciones menores

--- app/src/main/java/com/apm/plant_planner/LoginActivity.kt
+++ app/src/main/java/com/apm/plant_planner/LoginActivity.kt
@@ -3,16 +3,42 @@ package com.apm.plant_planner
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val registerBtn: Button = findViewById(R.id.register_btn)
+        // Version de Anton. TODO: revisar
+        /* val registerBtn: Button = findViewById(R.id.register_btn)
         registerBtn.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
+        */
+    }
+
+    fun register(view: View) {
+        Toast.makeText(applicationContext, "DEBUG: Register.", Toast.LENGTH_SHORT)
+        startActivity(Intent(this, RegisterActivity::class.java))
+    }
+
+    fun logIn(view: View) {
+        // Actualmente se usar un val y no se mete directamente porque luego habra mas datos
+        // de inicio de sesion
+        Toast.makeText(applicationContext, "Successfully log in.", Toast.LENGTH_SHORT)
+
+        val intent = Intent(this, EmptyInventoryActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun logInTwitter(view: View) {
+        Toast.makeText(applicationContext, "Not implemented yet.", Toast.LENGTH_SHORT)
+    }
+
+    fun logInGoogle(view: View) {
+        Toast.makeText(applicationContext, "Not implemented yet.", Toast.LENGTH_SHORT)
     }
 }
