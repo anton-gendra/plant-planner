3ce87014a993da8cd917cc9d696a4f7282791be9
agr17 <aregueiro1700@gmail.com>
2023-03-23 15:33:33 +0100

    Flujos desde Empty Inventory (apaño)

--- app/src/main/java/com/apm/plant_planner/EmptyInventoryActivity.kt
+++ app/src/main/java/com/apm/plant_planner/EmptyInventoryActivity.kt
@@ -3,6 +3,9 @@ package com.apm.plant_planner
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 
@@ -19,5 +22,20 @@ class EmptyInventoryActivity : AppCompatActivity() {
 
     fun openCamera(view: View) {
         Toast.makeText(applicationContext, "DEBUG: OpenCamera not implemented yet.", Toast.LENGTH_SHORT).show()
+    }
+
+    fun calendar(view: View) {
+        Toast.makeText(applicationContext, "DEBUG: Calendar.", Toast.LENGTH_SHORT).show()
+        startActivity(Intent(this, Calendar::class.java))
+    }
+
+    fun social(view: View) {
+        Toast.makeText(applicationContext, "DEBUG: Social.", Toast.LENGTH_SHORT).show()
+        startActivity(Intent(this, Social::class.java))
+    }
+
+    fun profile(view: View) {
+        Toast.makeText(applicationContext, "DEBUG: Calendar.", Toast.LENGTH_SHORT).show()
+        startActivity(Intent(this, Profile::class.java))
     }
 }
