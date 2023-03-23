8dd382079f0665679c7138a6f242c784fb9f8e19
Julio <julio.ffernandez@udc.es>
2023-03-23 19:10:23 +0100

    Flujo Inventory, Map Picker, PlantAttributes y SearchPlant

--- app/src/main/java/com/apm/plant_planner/PlantAtributtes.kt
+++ app/src/main/java/com/apm/plant_planner/PlantAtributtes.kt
@@ -1,11 +1,38 @@
 package com.apm.plant_planner
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Camera
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class PlantAtributtes : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plant_atributtes)
+        val changepicBtn: Button = findViewById(R.id.button)
+        changepicBtn.setOnClickListener {
+            Toast.makeText(this, "Change plant picture", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, Camera::class.java))
+        }
+        val changeMapLocationBtn: Button = findViewById(R.id.button2)
+        changeMapLocationBtn.setOnClickListener {
+            Toast.makeText(this, "Change map location", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, MapPicker::class.java))
+        }
+        val discardBtn: Button = findViewById(R.id.discard_btn)
+        discardBtn.setOnClickListener {
+            Toast.makeText(this, "Discard plant attributes", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, Inventory::class.java))
+        }
+        val addPlantBtn: Button = findViewById(R.id.button4)
+        addPlantBtn.setOnClickListener {
+            Toast.makeText(this, "Add the new plant", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, Inventory::class.java))
+        }
     }
+
 }
--- app/src/main/java/com/apm/plant_planner/SearchPlant.kt
+++ app/src/main/java/com/apm/plant_planner/SearchPlant.kt
@@ -1,11 +1,35 @@
 package com.apm.plant_planner
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class SearchPlant : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_plant)
+        val plantcardBtn: Button = findViewById(R.id.card_view)
+        plantcardBtn.setOnClickListener {
+            Toast.makeText(this, "Select the plant", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, PlantAtributtes::class.java))
+        }
+        val plantcard2Btn: Button = findViewById(R.id.card_view2)
+        plantcard2Btn.setOnClickListener {
+            Toast.makeText(this, "Select the plant", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, PlantAtributtes::class.java))
+        }
+        val plantcard3Btn: Button = findViewById(R.id.card_view3)
+        plantcard3Btn.setOnClickListener {
+            Toast.makeText(this, "Select the plant", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, PlantAtributtes::class.java))
+        }
+        val plantcard4Btn: Button = findViewById(R.id.card_view4)
+        plantcard4Btn.setOnClickListener {
+            Toast.makeText(this, "Select the plant", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, PlantAtributtes::class.java))
+        }
     }
+
 }
