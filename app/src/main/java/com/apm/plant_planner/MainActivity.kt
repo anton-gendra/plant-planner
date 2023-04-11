e3801ddbb69e3fd9dfa9c7c0a35fddc1b3d058f1
agr17 <aregueiro1700@gmail.com>
2023-04-11 19:00:30 +0200

    Fragment inicial de Inventario y transición entre fragments

--- app/src/main/java/com/apm/plant_planner/MainActivity.kt
+++ app/src/main/java/com/apm/plant_planner/MainActivity.kt
@@ -2,10 +2,37 @@ package com.apm.plant_planner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.apm.plant_planner.ui.CalendarFragment
+import com.apm.plant_planner.ui.EmptyInventoryFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav.setOnNavigationItemSelectedListener(navListener)
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container,
+            EmptyInventoryFragment()).commit()
+    }
+
+    private val navListener = BottomNavigationView.OnNavigationItemSelectedListener {
+        lateinit var selectedFragment: Fragment
+        when (it.itemId) {
+            R.id.page_inventory -> {
+                Toast.makeText(applicationContext, "DEBUG: Inventory.", Toast.LENGTH_SHORT).show()
+                selectedFragment = EmptyInventoryFragment()
+            }
+            R.id.page_calendar -> {
+                Toast.makeText(applicationContext, "DEBUG: Calendar.", Toast.LENGTH_SHORT).show()
+                selectedFragment = CalendarFragment()
+            }
+        }
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit()
+        true
     }
 }
