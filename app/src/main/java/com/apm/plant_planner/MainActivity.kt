3ccb2c1007e414d85cba986980813097de10efd9
agr17 <aregueiro1700@gmail.com>
2023-04-12 10:06:39 +0200

    Fragments iniciales para Camara y Perfil

--- app/src/main/java/com/apm/plant_planner/MainActivity.kt
+++ app/src/main/java/com/apm/plant_planner/MainActivity.kt
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.apm.plant_planner.ui.CalendarFragment
-import com.apm.plant_planner.ui.EmptyInventoryFragment
-import com.apm.plant_planner.ui.SocialFragment
+import com.apm.plant_planner.ui.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -25,21 +23,19 @@ class MainActivity : AppCompatActivity() {
         lateinit var selectedFragment: Fragment
         when (it.itemId) {
             R.id.page_inventory -> {
-                Toast.makeText(applicationContext, "DEBUG: Inventory.", Toast.LENGTH_SHORT).show()
                 selectedFragment = EmptyInventoryFragment()
             }
             R.id.page_calendar -> {
-                Toast.makeText(applicationContext, "DEBUG: Calendar.", Toast.LENGTH_SHORT).show()
                 selectedFragment = CalendarFragment()
             }
             R.id.page_camera -> {
-                Toast.makeText(applicationContext, "Camera not implemented yet.", Toast.LENGTH_SHORT).show()
+                selectedFragment = CameraFragment()
             }
             R.id.page_social -> {
                 selectedFragment = SocialFragment()
             }
-            R.id.page_camera -> {
-                Toast.makeText(applicationContext, "Profile not implemented yet.", Toast.LENGTH_SHORT).show()
+            R.id.page_profile -> {
+                selectedFragment = ProfileFragment()
             }
         }
         supportFragmentManager.beginTransaction().replace(R.id.navHostFragment, selectedFragment).commit()
