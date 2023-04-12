3983c3d6f90a15980617cf0109aa15ceeed9906a
Pablo <pabrod17@gmail.com>
2023-04-12 18:23:32 +0200

    Add link from social fragment to post activity

--- app/src/main/java/com/apm/plant_planner/ui/SocialFragment.kt
+++ app/src/main/java/com/apm/plant_planner/ui/SocialFragment.kt
@@ -1,11 +1,15 @@
 package com.apm.plant_planner.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.apm.plant_planner.Post
 import com.apm.plant_planner.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 /**
  * A simple [Fragment] subclass.
@@ -17,6 +21,16 @@ class SocialFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_social, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_social, container, false)
+
+        view.findViewById<FloatingActionButton>(R.id.floatingActionButton8).setOnClickListener {
+            Log.d("btnSetup", "Selected")
+            view.context.startActivity(Intent(view.context, Post::class.java))
+
+        }
+
+        return view;
+        // Inflate the layout for this fragment
+        //return inflater.inflate(R.layout.fragment_social, container, false)
     }
 }
