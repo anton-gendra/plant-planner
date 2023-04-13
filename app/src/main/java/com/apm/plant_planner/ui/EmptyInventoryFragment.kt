c0cbd8013ea7689701a9fc6f474d80c0fcb34c97
Julio <julio.ffernandez@udc.es>
2023-04-13 20:38:09 +0200

    Fixed bottom navigation view search plant

--- app/src/main/java/com/apm/plant_planner/ui/EmptyInventoryFragment.kt
+++ app/src/main/java/com/apm/plant_planner/ui/EmptyInventoryFragment.kt
@@ -1,11 +1,13 @@
 package com.apm.plant_planner.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.apm.plant_planner.R
+import com.apm.plant_planner.SearchPlant
 
 class EmptyInventoryFragment : Fragment() {
     override fun onCreateView(
@@ -14,5 +16,11 @@ class EmptyInventoryFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_empty_inventory, container, false)
+    }
+
+
+
+    fun searchPlant(view: View) {
+        startActivity(Intent(this.requireContext(), SearchPlant::class.java))
     }
 }
