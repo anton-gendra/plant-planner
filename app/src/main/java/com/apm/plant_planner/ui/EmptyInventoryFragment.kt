261793456fc4792bf04afee44e7c574a4d9165dd
Julio <julio.ffernandez@udc.es>
2023-04-13 23:07:45 +0200

    Hotfix SearchPlant

--- app/src/main/java/com/apm/plant_planner/ui/EmptyInventoryFragment.kt
+++ app/src/main/java/com/apm/plant_planner/ui/EmptyInventoryFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.apm.plant_planner.Inventory
 import com.apm.plant_planner.R
 import com.apm.plant_planner.SearchPlant
 
@@ -14,13 +16,12 @@ class EmptyInventoryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_empty_inventory, container, false)
-    }
-
-
-
-    fun searchPlant(view: View) {
-        startActivity(Intent(this.requireContext(), SearchPlant::class.java))
+        val view = inflater.inflate(R.layout.fragment_empty_inventory, container, false)
+        val searchButton: Button = view.findViewById(R.id.searchView)
+        searchButton.setOnClickListener {
+            val intent = Intent(requireActivity(), SearchPlant::class.java)
+            startActivity(intent)
+        }
+        return view
     }
 }
