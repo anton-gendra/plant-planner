f0f935ba429b2ed3b2a8dbb40ea6d0cd3152f831
Julio <julio.ffernandez@udc.es>
2023-04-13 19:44:32 +0200

    SearchFragment

--- app/src/main/java/com/apm/plant_planner/SearchPlant.kt
+++ app/src/main/java/com/apm/plant_planner/SearchPlant.kt
@@ -6,30 +6,19 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.apm.plant_planner.ui.SearchFragment
 
 class SearchPlant : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_plant)
-        val plantcardBtn: ConstraintLayout = findViewById(R.id.card_view)
-        plantcardBtn.setOnClickListener {
-            Toast.makeText(this, "Select the plant", Toast.LENGTH_LONG).show()
-            startActivity(Intent(this, PlantAtributtes::class.java))
-        }
-        val plantcard2Btn: ConstraintLayout = findViewById(R.id.card_view2)
-        plantcard2Btn.setOnClickListener {
-            Toast.makeText(this, "Select the plant", Toast.LENGTH_LONG).show()
-            startActivity(Intent(this, PlantAtributtes::class.java))
-        }
-        val plantcard3Btn: ConstraintLayout = findViewById(R.id.card_view3)
-        plantcard3Btn.setOnClickListener {
-            Toast.makeText(this, "Select the plant", Toast.LENGTH_LONG).show()
-            startActivity(Intent(this, PlantAtributtes::class.java))
-        }
-        val plantcard4Btn: ConstraintLayout = findViewById(R.id.card_view4)
-        plantcard4Btn.setOnClickListener {
-            Toast.makeText(this, "Select the plant", Toast.LENGTH_LONG).show()
-            startActivity(Intent(this, PlantAtributtes::class.java))
+
+        supportFragmentManager.commit {
+            replace<SearchFragment>(R.id.frameContainer)
+            setReorderingAllowed(true)
+            addToBackStack("replacement")
         }
     }
 
