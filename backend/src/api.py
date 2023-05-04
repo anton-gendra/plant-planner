a7a87d376f91fba0768f125f4e9bafc11c790afb
anton <gendra.job@gmail.com>
2023-05-04 23:44:36 +0200

    Hardcode admin credentials in order to test de app. Add multiDex so we can bypass the dex limit

--- backend/src/api.py
+++ backend/src/api.py
@@ -30,7 +30,9 @@ def info():
 def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     raw_user = db.get_user_by_username(form_data.username)
     user = User(id=raw_user[0], name=raw_user[1], password=raw_user[2])
-    if user.password == form_data.password:
+    if user.password == form_data.password or (form_data.username.lower() == 'admin' and form_data.password.lower() == 'admin'):
+        if form_data.username.lower() == 'admin' and form_data.password.lower() == 'admin':
+            return {'access_token': session_manager.new_session(-1), 'token_type': "bearer"}
         return {'user': user, 'access_token': session_manager.new_session(user.id), 'token_type': "bearer"}
     
     else:
