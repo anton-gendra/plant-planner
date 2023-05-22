7a8ef1d1dbcba4e3e249f1d4016ed22534296349
Pablo <pabrod17@gmail.com>
2023-05-22 20:55:30 +0200

    Add support to save post image base64

--- app/src/main/java/com/apm/plant_planner/ui/PostFragment.kt
+++ app/src/main/java/com/apm/plant_planner/ui/PostFragment.kt
@@ -1,23 +1,28 @@
 package com.apm.plant_planner.ui
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Base64
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import androidx.activity.result.contract.ActivityResultContracts
+import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.apm.plant_planner.LoginActivity
 import com.apm.plant_planner.Maps
 import com.apm.plant_planner.R
 import org.json.JSONObject
+import java.io.ByteArrayOutputStream
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,6 +39,8 @@ class PostFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
     private var title: String = ""
+    private var bitmap: Bitmap? = null
+    private var base64Image: String = ""
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,12 +52,35 @@ class PostFragment : Fragment() {
         }
     }
 
+    // Get your image
+    private val resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                if (result?.data != null && view != null) {
+                    bitmap = result.data?.extras?.get("data") as Bitmap
+                    val plant_image = view?.findViewById<ImageView>(R.id.imageView2)
+                    plant_image?.setImageBitmap(bitmap)
+                }
+            }
+        }
+
+    @SuppressLint("WrongThread")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
 
     ): View? {
         val view: View = inflater.inflate(R.layout.fragment_post, container, false)
+
+        val btnTakePhoto: Button? = view.findViewById(R.id.camaraPost)
+
+        // Boton para la camara
+        if (btnTakePhoto != null) {
+            btnTakePhoto.setOnClickListener {
+                val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                resultLauncher.launch(cameraIntent)
+            }
+        }
 
         val editTitle = view?.findViewById<EditText>(R.id.editTextTitle);
         view.findViewById<Button>(R.id.addLocationPost).setOnClickListener {
@@ -76,34 +106,47 @@ class PostFragment : Fragment() {
 
         val publishButton: Button = view.findViewById(R.id.publishButton)
         publishButton.setOnClickListener {
-            Toast.makeText(context, "Title: $param2 - Location: $param1", Toast.LENGTH_SHORT).show()
-            //llamada al back! hacer en post con todos los datos, no aqui solo con las coordenadas
+            //Comprobar imagen
+            if (bitmap != null) {
+                val outputStream = ByteArrayOutputStream()
+                bitmap!!.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+                val imageByteArray: ByteArray = outputStream.toByteArray()
+                base64Image = Base64.encodeToString(imageByteArray, Base64.DEFAULT)
 
+            }
+
+            Toast.makeText(context, "Title: $param2 - Location: $param1", Toast.LENGTH_SHORT).show()
 
             val queue = Volley.newRequestQueue(context)
             val url = "http://10.0.2.2:8000/plant/post"
 
             val body = JSONObject()
-            body.put("title", param1)
-            body.put("location", param2)
-            body.put("image", "image1")
+            body.put("title", param2)
+            body.put("location", param1)
+            body.put("image", base64Image)
             body.put("author", 1)
 
-
-            val request = object: JsonObjectRequest(
-                Method.POST, url, body,
+            val request = JsonObjectRequest(
+                Request.Method.POST, url, body,
                 { response ->
-
-                    Toast.makeText(context, "Register data: ".plus(response.toString()), Toast.LENGTH_SHORT).show()
-
+                    Toast.makeText(context, "Post data: " + response.toString(), Toast.LENGTH_SHORT).show()
                 },
                 { error ->
                     Toast.makeText(context, error.toString(), Toast.LENGTH_SHORT).show()
                 }
-            ) {}
+            )
 
             queue.add(request)
+
         }
+
+        //AQUI COJO EL BASE64 DE LA BD Y MUESTRA LA IMAGEN EN EL OBJ XML DE IMAGEVIEW
+        //HACERLO EN SOCIAL
+        /*val imageByteArray = Base64.decode(base64Image, Base64.DEFAULT)
+        val bitmap2 = BitmapFactory.decodeByteArray(imageByteArray, 0, imageByteArray.size)
+        val imageView: ImageView = view.findViewById(R.id.imageView3)
+        imageView.setImageBitmap(bitmap2)*/
+
         // Inflate the layout for this fragment
 
 
