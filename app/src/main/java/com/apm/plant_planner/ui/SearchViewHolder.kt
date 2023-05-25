1a2366b7834cc0b7ac4e03373e624b095280f2f4
Julio <julio.ffernandez@udc.es>
2023-05-25 19:26:48 +0200

    Preparing images to next updates [API limitation to 100 pics]

--- app/src/main/java/com/apm/plant_planner/ui/SearchViewHolder.kt
+++ app/src/main/java/com/apm/plant_planner/ui/SearchViewHolder.kt
@@ -1,15 +1,23 @@
 package com.apm.plant_planner.ui
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.apm.plant_planner.R
+import com.squareup.picasso.Picasso
+import java.net.URL
 
 class SearchViewHolder(itemView: View):RecyclerView.ViewHolder(itemView) {
 
     val plantName = itemView.findViewById<TextView>(R.id.textView18)
+    //val plantimage = itemView.findViewById<ImageView>(R.id.imageView3)
 
     fun create_items(searchItemModel: SearchItem) {
         plantName.text = searchItemModel.plantName
+      /*  Picasso.get().load(searchItemModel.plantImage).into(plantimage)*/
     }
+
+
+
 }
