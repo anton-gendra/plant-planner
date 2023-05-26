e177d54b8b81b210fb560190810c913e6353ab1d
agr17 <aregueiro1700@gmail.com>
2023-05-26 13:57:59 +0200

    Diseño y funcionamiento final para CalendarFragment

--- app/src/main/java/com/apm/plant_planner/ui/CalendarFragment.kt
+++ app/src/main/java/com/apm/plant_planner/ui/CalendarFragment.kt
@@ -4,26 +4,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.apm.plant_planner.R
+import java.text.SimpleDateFormat
+import java.util.*
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CalendarFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CalendarFragment : Fragment(R.layout.fragment_calendar) {
-    // TODO: Rename and change types of parameters
-//    private var param1: String? = null
-//    private var param2: String? = null
-//
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        arguments?.let {
-//            param1 = it.getString(ARG_PARAM1)
-//            param2 = it.getString(ARG_PARAM2)
-//        }
-//    }
+
+    private lateinit var calendarView: CalendarView
+    private lateinit var textNoEvents: TextView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        calendarView = view.findViewById(R.id.calendarView)
+        textNoEvents = view.findViewById(R.id.textNoEvents)
+
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            val selectedDate = Calendar.getInstance()
+            selectedDate.set(year, month, dayOfMonth)
+            val formattedDate = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(selectedDate.time)
+            val actualDate = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(Date())
+
+            if (formattedDate == actualDate) {
+                textNoEvents.text = getString(R.string.no_events_today)
+            } else if (selectedDate.time.before(Date())) {
+                textNoEvents.text = getString(R.string.no_events_past, formattedDate)
+            } else {
+                textNoEvents.text = getString(R.string.no_events_future, formattedDate)
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,24 +45,4 @@ class CalendarFragment : Fragment(R.layout.fragment_calendar) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calendar, container, false)
     }
-
-//    companion object {
-//        /**
-//         * Use this factory method to create a new instance of
-//         * this fragment using the provided parameters.
-//         *
-//         * @param param1 Parameter 1.
-//         * @param param2 Parameter 2.
-//         * @return A new instance of fragment CalendarFragment.
-//         */
-//        // TODO: Rename and change types and number of parameters
-//        @JvmStatic
-//        fun newInstance(param1: String, param2: String) =
-//            CalendarFragment().apply {
-//                arguments = Bundle().apply {
-//                    putString(ARG_PARAM1, param1)
-//                    putString(ARG_PARAM2, param2)
-//                }
-//            }
-//    }
 }
